523a1a56976173f5d89cd8773f88270b3b84d3c4
Alberto Hernandez <hernalberto16@gmail.com>
2025-04-10 19:04:50 -0400

    Completed the design for the home page.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import '../styles/FooterStyles.css'
 
+import resume from '../assets/Alberto_S_Hernandez_Resume.pdf'
+import LetterOfRec from '../assets/ASH_UKG_LetterOfReccomendation.pdf'
+
 function Footer() {
     return (
       <div className="footer">
@@ -7,20 +10,20 @@ function Footer() {
 
           <div class="footerDivision">
             <h3 class="divisionTitle">Contacts</h3>
-            <a href="mailto:albertohern1116@gmail.com" className='linkTxt'>Email</a>
+            <a href="mailto:albertohern1116@gmail.com" className='linkTxt'>albertohern1116@gmail.com</a>
             <h3 className='linkTxt'>Mobile Number: (786) 479-1686</h3>
           </div>
 
           <div class="footerDivision">
             <h3 class="divisionTitle">External Sites</h3>
-            <a href="https://www.linkedin.com/in/albertoh16/" className='linkTxt'>LinkedIn</a>
-            <a href="https://github.com/Albertoh16" className='linkTxt'>GitHub</a>
+            <a href="https://www.linkedin.com/in/albertoh16/" className='linkTxt' target="_blank">LinkedIn</a>
+            <a href="https://github.com/Albertoh16" className='linkTxt' target="_blank">GitHub</a>
           </div>
 
           <div class="footerDivision">
             <h3 class="divisionTitle">Documents</h3>
-            <a href="../assets/Alberto_S_Hernandez_Resume.pdf" className='linkTxt'>Resume</a>
-            <a href="../assets/LetterOfReccomendation.pdf" className='linkTxt'>UKG Letter of Reccomendation</a>
+            <a href={resume} className='linkTxt' download='Alberto_S_Hernandez_Resume.pdf'>Resume</a>
+            <a href={LetterOfRec} className='linkTxt' download='ASH_UKG_LetterOfReccomendation.pdf'>UKG Letter of Reccomendation</a>
           </div>
 
         </div>
