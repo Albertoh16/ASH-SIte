4fafdf6a537c3f23ff44a5d3959104955395cd21
Alberto Hernandez <hernalberto16@gmail.com>
2025-04-10 13:18:27 -0400

    Began designing footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,27 @@ import '../styles/FooterStyles.css'
 function Footer() {
     return (
       <div className="footer">
+        <div class="footerContainer">
+
+          <div class="footerDivision" id='division1'>
+            <h3 class="divisionTitle">
+              Contacts
+            </h3>
+          </div>
+
+          <div class="footerDivision" id='division2'>
+            <h3 class="divisionTitle">
+              External Sites
+            </h3>
+          </div>
+
+          <div class="footerDivision" id='division3'>
+            <h3 class="divisionTitle">
+              Documents
+            </h3>
+          </div>
+
+        </div>
       </div>
     )
   }
